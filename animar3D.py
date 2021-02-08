78f67bd781857c49afad40dbd67d614ab55de5ff
oscarmonllor99 <78754527+oscarmonllor99@users.noreply.github.com>
2021-02-08 22:43:17 +0100

    Update animar3D.py

--- animar3D.py
+++ animar3D.py
@@ -1,17 +1,16 @@
-# -*- coding: utf-8 -*-
+ -*- coding: utf-8 -*-
 """
 Created on Fri Jul 10 17:58:16 2020
 
 @author: Oscar
 """
 
-
+from numba import jit
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import matplotlib.animation as animation
-import mpl_toolkits.mplot3d.axes3d as p3
-import matplotlib
+
 
 ###########################################
 ##########################################
@@ -28,8 +27,11 @@ save = True
 Parámetros físicos
 
 """""""""""""""
+M = 3245*2.325*1e7#masa total de las particulas q van a interactuar
 
-N = 100 #Número de partículas
+N = 1000 #Número de partículas
+
+m = M / N #masa de las particulas en Msolares
 
 T_sol = 225 #periodo del Sol alrededor de la galaxia
 
@@ -37,16 +39,20 @@ T_sol = 225 #periodo del Sol alrededor de la galaxia
 Parámetros de simulación
 
 """""""""""""""
-salt = 1 #salto en los pasos, en vez de ir de 1 paso en 1 paso, se va de salt en salt para agilizar y ahorrar espacio
+salt = 4 
 
-lim = 100 #en kpc
+lim = 100 
 
 x_lim, y_lim, z_lim = lim, lim, lim
 
 n = 400
-dt = (T_sol / 2000) #intervalo de tiempo entre cada paso
+dt = (T_sol / 2000) 
+N_p = 200
+n_p = N_p 
+m_p = N_p 
 
-r = 0.01 #en kpc
+hx = x_lim / n_p #distancia entre puntos de red eje X
+hy = y_lim / m_p #distancia entre puntos de red eje Y
 
 tray = np.loadtxt('trayectorias.dat', dtype = float)
 tray_3D = tray.reshape(n, N, 3)
@@ -56,51 +62,54 @@ tray_3D = tray.reshape(n, N, 3)
 
 #creamos la figura
 fig = plt.figure()
-ax = p3.Axes3D(fig)
-ax.azim = 180
-ax.elev = 50
-
-ax.set_xlim([-x_lim/2, x_lim/2])
-ax.set_xlabel('X (kpc)')
-    
-ax.set_ylim([-y_lim/2, y_lim/2])
-ax.set_ylabel('Y (kpc)')
-    
-ax.set_zlim([-z_lim/2 , z_lim/2])
-ax.set_zlabel('Z (kpc)')
-
-ax.set_facecolor('white')
-
-ax.set_axis_off()
-
-#dibujamos las esferas que representan a cada partícula
-    
-X = np.empty(N)  
-Y = np.copy(X)
-Z = np.copy(X)
+ax = plt.axes(xlim = (0, x_lim), ylim = (0, y_lim))
 
 #posiciones iniciales de las partículas
 #tray[paso de tiempo, particula, eje]
-X[:], Y[:], Z[:] = tray_3D[0, :, 0], tray_3D[0, :, 1], tray_3D[0, :, 2],
+@jit(nopython=True, fastmath = True)
+def densidad(r_list_0):
+    rho = np.zeros((n_p, m_p))
+    for i in range(N):
+        x_pos = int(r_list_0[i,0] / hx)
+        y_pos = int(r_list_0[i,1] / hy)
+        
+        if x_pos <= 0 or x_pos >= N_p or y_pos <= 0 or y_pos >= N_p:
+            
+            pass
+        
+        else:
+        
+            rho[x_pos, y_pos] += m / (hx*hy)
 
-particulas, = ax.plot(X, Y, Z, marker = 'd', c = 'black', markersize = 10*r, linestyle='None')
-plt.grid()
+    return rho
+
+x = np.arange(0, x_lim, hx)
+y = np.arange(0, y_lim, hy)
+
+X, Y = np.meshgrid(x,y)
+
+@jit(nopython=True, fastmath = True)
+def iterator_rho(rho):
+    for k in range(n):
+        rho[k, :, :] = densidad(tray_3D[k, :, :])
+    return rho
+
+rho = iterator_rho(np.empty((n, n_p, m_p)))
+    
+
+    
+contour = ax.contourf(X,Y, rho[0,:,:], cmap = 'nipy_spectral', levels = 300)
 txt = fig.suptitle('')
-       
+colorbar = fig.colorbar(contour)
+
+
 def animation_frame(k):
         
-        txt.set_text('{:f} millones de años'.format(k*25*dt))
-        
-        #se crean x_data e y_data donde se almacenan las posiciones para cada paso k de tiempo
-        
-        X = tray_3D[k, :, 0]- x_lim/2
-        Y = tray_3D[k, :, 1] - y_lim/2
-        Z = tray_3D[k, :, 2] - z_lim/2
-
-        particulas.set_data(X, Y)
-        particulas.set_3d_properties(Z, 'z')
-
-        return particulas, txt
+    txt.set_text('{:f} millones de años'.format(k*25*dt))
+    
+    ax.contourf(X,Y, rho[k,:,:], cmap = 'nipy_spectral', levels = 60)
+    
+    return txt
             
     
 movimiento = FuncAnimation(fig, func=animation_frame, frames=np.arange(1, n, salt),repeat=True,
@@ -110,5 +119,5 @@ movimiento = FuncAnimation(fig, func=animation_frame, frames=np.arange(1, n, sal
 if save:
         # Set up formatting for the movie files
         Writer = animation.writers['ffmpeg']
-        writer = Writer(fps=20, metadata=dict(artist='Me'), bitrate=1800)
-        movimiento.save('movimiento.mp4', writer=writer)
+        writer = Writer(fps=30, metadata=dict(artist='Me'), bitrate=1800)
+        movimiento.save('movimiento_surface.mp4', writer=writer)
